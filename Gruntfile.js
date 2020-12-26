c4e287ac5cbf8d20c2c1873b8d88f40fe7a96b95
JuanDavid24 <david.juan90.jd@gmail.com>
2020-12-25 21:47:08 -0300

    +browserSync y imagemin para grunt agregados

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,20 +4,50 @@ module.exports = function(grunt) {
             dist: {
                 files: [{
                     expand: true,
-                    cwd: 'css',
-                    src: ['*.scss'],
-                    dest: 'css',
-                    ext: '.css'
+                    cwd: 'css',         //carpeta origen 
+                    src: ['*.scss'],    //origen: todos los .scss 
+                    dest: 'css',        //carpeta destino
+                    ext: '.css'         //extension
                 }]
             }
         },
         watch: {
-            files: ['css/*.scss'],
-            tasks: ['sass']
-        }
+            files: ['css/*.scss'],      //mirar cambios en
+            tasks: ['sass']             //la tarea de grunt que se ejecuta
+        },
+        browserSync: {
+            dev: {
+                bsFiles: {              //browser files
+                    src: [              //archivos que tiene que mirar
+                        'css/*.css',
+                        '*.html',
+                        'js/*.js']
+                },
+                options: {
+                    watchTask: true,
+                    server: {
+                        baseDir: './'   //Directorio base para nuestro servidor
+                    }
+                }
+            }
+        },
+        imagemin: {
+            dynamic: {
+                files: [{
+                    expand: true,
+                    cwd: './',
+                    src: 'images/*.{png,gif,jpg,jpeg}',
+                    dest: 'dist/'
+                }]
+            }
+        },
     });
 
-    grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-sass'); //cargar plug-in
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-browser-sync');
+
+    //definimos las tareas de grunt (las que se ejecutan con 'grunt [nombreDeTarea]')
     grunt.registerTask('css', ['sass']);
+    grunt.registerTask('default', ['browserSync', 'watch']); //tarea default (solo ejecutando 'grunt' a secas)
 }
